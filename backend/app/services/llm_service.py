4ba78d3d35321bc31b8cf102a45d46054ede76e9
yashvi-jain <yashvijain2610@gmail.com>
2026-09-10 00:13:35 +0530

    replaced llm model, due to discontinuation of previous one

--- backend/app/services/llm_service.py
+++ backend/app/services/llm_service.py
@@ -13,7 +13,7 @@ client = OpenAI(
 def query_llm(prompt):
 
     response = client.chat.completions.create(
-        model="llama-3.1-8b-instant",
+        model="openai/gpt-oss-20b",
         messages=[
             {
                 "role": "user",
